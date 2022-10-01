1fb528b070e58948a467865393e6c9504d0f14a7
lhwsam <61182188+lhwsam@users.noreply.github.com>
2022-10-01 16:38:09 +0900

    test

--- BlockMulti/Assets/Scripts/Test.cs
+++ BlockMulti/Assets/Scripts/Test.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hi");
     }
 
     // Update is called once per frame
