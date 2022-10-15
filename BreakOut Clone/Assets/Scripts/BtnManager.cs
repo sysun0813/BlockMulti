68ccbde8f89950b820ea07e8490ae828c0471f66
sysun0813 <sysun0813@naver.com>
2022-10-15 18:23:04 +0900

    메인 메뉴 UI 배치 및 기능 제작

---
+++ BreakOut Clone/Assets/Scripts/BtnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour {
+
+	public GameObject single;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SingleButtonOn()
+    {
+		SceneManager.LoadScene("SampleScene");
+    }
+
+	public void ExitButtonOn()
+    {
+//#if UNITY_EDITOR
+//			UnityEditor.EditorApplication.isPlaying = false;
+//#elif UNITY_IOS
+		
+//#else
+//		Application.Quit(0);
+//#endif
+	}
+}
